5ac77354411dd6f082cc6ea399e2191664c5ce11
Zygintas <zygintas.misiunas@stud.vilniustech.lt>
2022-06-02 21:02:04 +0300

    Add project files.

---
+++ ReizZygintas/Branch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ReizZygintas
+{
+    class Branch
+    {
+       public List<Branch> branches = new List<Branch>();
+       public string _data;
+
+        public Branch(string data)
+        {
+            _data = data;
+        }
+
+       public int Depth
+        {
+            get
+            {
+                int depth = 0;
+                foreach (Branch branch in branches)
+                {
+                    var descendantDepth = branch.Depth;
+                    if (descendantDepth > depth)
+                        depth = descendantDepth;
+                }
+                depth++;
+                Console.WriteLine($"{_data} -- {depth}");
+                return depth;
+            }
+        }
+    }
+}
---
+++ ReizZygintas/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace ReizZygintas
+{
+    class Program
+    {
+        public static Branch root = new Branch("Root");
+        static void Main(string[] args)
+        {
+            //1
+
+            /*
+            double hourAngle,minuteAngle,angle;
+
+            Console.WriteLine("Enter hours:");
+            int.TryParse(Console.ReadLine(),out var hours);
+            Console.WriteLine("Enter minutes:");
+            int.TryParse(Console.ReadLine(), out var minutes);
+
+            hourAngle = (hours * 30) + (minutes / 2.0);
+            minuteAngle = minutes * 6;
+            angle = Math.Abs(hourAngle - minuteAngle);
+
+            if(angle > 180)
+            {
+                angle = 360 - angle;
+            }
+
+            Console.WriteLine($"Angle between hours and minutes is: {angle}");
+            */
+
+            //2-3
+            Branch branch1 = new Branch("branch1");
+            Branch branch11 = new Branch("branch11");
+            Branch branch111 = new Branch("branch111");
+            Branch branch2 = new Branch("branch2");
+            Branch branch21 = new Branch("branch21");
+            Branch branch211 = new Branch("branch211");
+            Branch branch22 = new Branch("branch22");
+            Branch branch221 = new Branch("branch221");
+            Branch branch2211 = new Branch("branch2211");
+            Branch branch222 = new Branch("branch222");
+            Branch branch23 = new Branch("branch23");
+
+            root.branches.Add(branch1);
+            root.branches.Add(branch2);
+            branch1.branches.Add(branch11);
+            branch11.branches.Add(branch111);
+            branch2.branches.Add(branch21);
+            branch21.branches.Add(branch211);
+            branch2.branches.Add(branch22);
+            branch22.branches.Add(branch221);
+            branch221.branches.Add(branch2211);
+            branch22.branches.Add(branch222);
+            branch2.branches.Add(branch23);
+
+            Console.WriteLine(root.Depth);
+        }
+    }
+}
